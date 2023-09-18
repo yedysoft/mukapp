94df01dc4f070bcc812a35dec5ffa1acbdb2f9f3
Yasin Şensoy <yasin_sensoy@hotmail.com>
2023-09-19 01:05:20 +0300

    .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 200,
+  bracketSpacing: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};
