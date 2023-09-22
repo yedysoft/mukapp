c55016bb3d531e1279a84d25ebc51dd6f9d631e7
Ethem <127316095+ethasl@users.noreply.github.com>
2023-09-22 13:02:45 +0300

    .

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -2,8 +2,6 @@ import {useTheme} from 'react-native-paper';
 import {ReactNode} from 'react';
 import {View} from 'react-native';
 import {observer} from 'mobx-react';
-import {stores} from '../../stores';
-import MukModal from '../custom/MukModal';
 
 type Props = {
   children: ReactNode;
@@ -12,10 +10,5 @@ type Props = {
 export const MainLayout = observer(({children}: Props) => {
   const {colors} = useTheme();
 
-  return (
-    <View style={{flex: 1, flexDirection: 'column'}}>
-      {children}
-      <MukModal onDismiss={() => stores.ui.set('tooltip', false)} visible={stores.ui.tooltip} />
-    </View>
-  );
+  return <View style={{flex: 1, flexDirection: 'column'}}>{children}</View>;
 });
--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -3,8 +3,8 @@ import {CombinedTheme} from '../theme';
 import {NavigationContainer} from '@react-navigation/native';
 import {observer} from 'mobx-react';
 import {stores} from '../stores';
-import StartDrawer from './StartDrawer';
+import MainDrawer from './MainDrawer';
 
 export const AppNavigation = observer(() => {
-  return <NavigationContainer theme={CombinedTheme}>{stores.auth.isLoggedIn ? <StartDrawer /> : <AuthStack />}</NavigationContainer>;
+  return <NavigationContainer theme={CombinedTheme}>{!stores.auth.isLoggedIn ? <MainDrawer /> : <AuthStack />}</NavigationContainer>;
 });
