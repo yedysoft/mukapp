d419db4ce1bc4063e7122fb5d42a3a0da38385ca
Yasin Şensoy <yasin_sensoy@hotmail.com>
2023-09-28 22:57:47 +0300

    .

--- src/services/api/subscription.ts
+++ src/services/api/subscription.ts
@@ -5,21 +5,34 @@ import {stores} from '../../stores';
 export class SubscriptionApi {
   async globalSubscribes(): PVoid {
     try {
-      const coinCallback = (message: Message) => {
-        const coin = JSON.parse(message.body);
-        stores.user.set('info', {...stores.user.getInfo, coin: coin});
-        console.log('coinCallback', coin);
-      };
-
-      const errorCallback = (message: Message) => {
-        console.log('errorCallback', message.body);
-      };
-
-      await socket.subscribe('/info/coin', coinCallback);
-      await socket.subscribe('/error', errorCallback);
+      await socket.subscribe('/info/coin', this.coinCallback);
+      await socket.subscribe('/error', this.errorCallback);
     } catch (e) {
       console.log(e);
     }
+  }
+
+  async roomSubscribes(streamerId: string): PVoid {
+    try {
+      await socket.subscribe(`/room/${streamerId}/playingTrack`, this.playingTrackCallback);
+      await socket.subscribe('/live/room/admin');
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  private coinCallback(message: Message) {
+    const coin = JSON.parse(message.body);
+    stores.user.set('info', {...stores.user.getInfo, coin: coin});
+    console.log('coinCallback', coin);
+  }
+
+  private errorCallback(message: Message) {
+    console.log('errorCallback', message.body);
+  }
+
+  private playingTrackCallback(message: Message) {
+    console.log('playingTrackCallback', message.body);
   }
 }
 
