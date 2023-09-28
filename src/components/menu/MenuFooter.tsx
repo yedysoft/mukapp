ac23f8dec3a7a0a6a96398e727626e42bbb5d004
etcas <76708790+etcas@users.noreply.github.com>
2023-09-28 22:12:46 +0300

    .

---
+++ src/components/menu/MenuFooter.tsx
@@ -0,0 +1,18 @@
+import {View} from 'react-native';
+import {useTheme} from 'react-native-paper';
+import {responsiveWidth} from '../../utils/Responsive';
+import MukIconButton from '../custom/MukIconButton';
+import {useServices} from '../../services';
+
+export default function MenuFooter() {
+  const {colors} = useTheme();
+  const {api} = useServices();
+
+  return (
+    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingLeft: responsiveWidth(16)}}>
+      <MukIconButton icon={'theme-light-dark'} onPress={() => api.auth.logout()} scale={.5} style={{}}/>
+      <MukIconButton icon={'logout'} onPress={() => api.auth.logout()} scale={.5} style={{}}
+                     color={'rgba(255, 55, 95, 1)'}/>
+    </View>
+  );
+}
--- src/screens/main/SideScreen.tsx
+++ src/screens/main/SideScreen.tsx
@@ -6,6 +6,7 @@ import MukIconButton from '../../components/custom/MukIconButton';
 import {responsiveWidth} from '../../utils/Responsive';
 import MenuList from '../../components/menu/MenuList';
 import {View} from 'react-native';
+import MenuFooter from '../../components/menu/MenuFooter';
 
 const menu = [
   {
@@ -37,17 +38,12 @@ const menu = [
 
 export default function SideScreen() {
   const {colors} = useTheme();
-  const {api} = useServices();
 
   return (
     <DrawerLayout>
       <HorizontalUser/>
       <MenuList menu={menu} />
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingLeft: responsiveWidth(16)}}>
-        <MukIconButton icon={'theme-light-dark'} onPress={() => api.auth.logout()} scale={.5} style={{}}/>
-        <MukIconButton icon={'logout'} onPress={() => api.auth.logout()} scale={.5} style={{}}
-                       color={'rgba(255, 55, 95, 1)'}/>
-      </View>
+      <MenuFooter />
     </DrawerLayout>
   );
 }
