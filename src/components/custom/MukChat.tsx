790aee67307c69b7bae1d51b15e7f109949bd2c7
etcas <76708790+etcas@users.noreply.github.com>
2023-09-29 22:41:12 +0300

    .

--- src/components/custom/MukChat.tsx
+++ src/components/custom/MukChat.tsx
@@ -1,10 +1,12 @@
 import {useTheme} from 'react-native-paper';
-import {GiftedChat, IMessage} from 'react-native-gifted-chat';
+import {Composer, GiftedChat, IMessage, InputToolbar, Send} from 'react-native-gifted-chat';
 import {useCallback, useState} from 'react';
 import {observer} from 'mobx-react';
 import {useStores} from '../../stores';
 import {useServices} from '../../services';
 import {Message} from '@stomp/stompjs';
+import MukIcon from './MukIcon';
+import {responsiveHeight, responsiveWidth} from '../../utils/Responsive';
 
 type Props = {
   subDestination: string;
@@ -29,6 +31,46 @@ export const MukChat = observer(({subDestination, sendDestination}: Props) => {
     api.socket.sendMessage(sendDestination, m);
   };
 
+  const renderInputToolbar = (props) => (
+    <InputToolbar
+      {...props}
+      containerStyle={{
+        backgroundColor: colors.background,
+        borderTopColor: colors.primary,
+        borderTopWidth: 1,
+        paddingTop: responsiveWidth(4),
+        paddingBottom: responsiveWidth(16),
+        paddingHorizontal: responsiveWidth(8)
+      }}
+      primaryStyle={{}}
+    />
+  )
+
+  const renderComposer = (props) => (
+    <Composer
+      {...props}
+      textInputAutoFocus
+      textInputStyle={{
+        color: 'white',
+      }}
+      placeholderTextColor={'white'}
+      placeholder=""
+    />
+  )
+
+  const renderSend = (props) => (
+    <Send
+      {...props}
+      disabled={!props.text}
+      containerStyle={{
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <MukIcon icon="send" scale={.6} color="white"/>
+    </Send>
+  )
+
   return (
     <GiftedChat
       messages={messages}
@@ -36,6 +78,10 @@ export const MukChat = observer(({subDestination, sendDestination}: Props) => {
       user={{
         _id: user.getInfo.id || '-1',
       }}
+      messagesContainerStyle={{paddingVertical: responsiveWidth(16)}}
+      renderInputToolbar={renderInputToolbar}
+      renderComposer={renderComposer}
+      renderSend={renderSend}
     />
   );
 });
