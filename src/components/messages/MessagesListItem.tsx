40201d3a04cadb55c5eb0332105eddda1cba517d
etcas <76708790+etcas@users.noreply.github.com>
2023-09-29 22:30:06 +0300

    .

--- src/components/messages/MessagesListItem.tsx
+++ src/components/messages/MessagesListItem.tsx
@@ -19,16 +19,16 @@ export default function MessagesListItem({onPress, chats}: Props) {
   return (
     <MukListItem onPress={onPress}>
       <MukImage scale={1.3} style={{borderRadius: 100, borderColor: colors.primary, borderWidth: 1}} source={require('../../../assets/adaptive-icon.png')}/>
-      <View style={{flex: 1, justifyContent: 'flex-start', gap: responsiveWidth(8)}}>
+      <View style={{flex: 1, justifyContent: 'flex-start', gap: responsiveWidth(8), paddingTop: responsiveWidth(8)}}>
         <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-          <Text numberOfLines={1} style={{fontSize: responsiveSize(17), fontWeight: '500'}}>
+          <Text numberOfLines={1} style={{fontSize: responsiveSize(18), fontWeight: '500'}}>
             {chats?.username}
           </Text>
-          <Text style={{fontSize: responsiveSize(14), fontWeight: '400', position: 'absolute', right: 0}}>
+          <Text numberOfLines={1} style={{fontSize: responsiveSize(15), fontWeight: '400', position: 'absolute', right: 0}}>
             {chats?.date}
           </Text>
         </View>
-        <Text numberOfLines={2} style={{flex: 1, fontSize: responsiveSize(14), fontWeight: '400'}}>
+        <Text numberOfLines={2} style={{flex: 1, fontSize: responsiveSize(15), fontWeight: '400'}}>
           {chats?.message}
         </Text>
       </View>
