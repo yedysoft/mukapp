93b83c80a427a9ea37c751b761e5a8f6f0b0a633
Ethem <127316095+ethasl@users.noreply.github.com>
2023-12-05 14:52:10 +0300

    rollback: fonts

--- App.tsx
+++ App.tsx
@@ -16,28 +16,11 @@ import {usePushNotifications} from './src/services/pushNotifications';
 import * as Device from 'expo-device';
 import {Appearance, NativeEventSubscription} from 'react-native';
 import {NavigationContainer, Theme} from '@react-navigation/native';
-import {useFonts} from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import themes from './src/themes';
-
-SplashScreen.preventAutoHideAsync();
 
 // noinspection JSUnusedGlobalSymbols
 export default observer(() => {
   const [ready, setReady] = useState(false);
   const [apperanceListener, setApperanceListener] = useState<NativeEventSubscription>();
-
-  const [fontsLoaded] = useFonts(themes.fonts);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
 
   Device.isDevice && usePushNotifications();
 
@@ -78,7 +61,7 @@ export default observer(() => {
           <StatusBar style={stores.ui.getStatusBarStyle} />
           <MessageStack />
           <DialogStack />
-          {!ready ? <MukSplashScreen /> : <AppNavigation onLayout={onLayoutRootView} />}
+          {!ready ? <MukSplashScreen /> : <AppNavigation />}
         </PaperProvider>
       </NavigationContainer>
     </AppProvider>
