620f56cdc565cab0917dba032b2fb5d4c6b591cb
Ethem <127316095+ethasl@users.noreply.github.com>
2024-03-19 16:55:58 +0300

    add:bottom-sheet

---
+++ src/components/custom/MukBottomSheet.tsx
@@ -0,0 +1,144 @@
+import {Portal, useTheme} from 'react-native-paper';
+import {Animated, PanResponder, Platform, StyleProp, View, ViewStyle} from 'react-native';
+import {MukTheme} from '../../types';
+import {useStores} from '../../stores';
+import {ReactNode, useEffect, useRef} from 'react';
+import {useServices} from '../../services';
+import {responsiveWidth} from '../../utils/util';
+
+type Props = {
+  style?: StyleProp<ViewStyle>;
+  visible: boolean;
+  setVisible: (a: boolean) => void;
+  children?: ReactNode;
+};
+export default function MukBottomSheet({style, visible, setVisible, children}: Props) {
+  const {colors} = useTheme<MukTheme>();
+  const {api} = useServices();
+  const {ui} = useStores();
+
+  const BOTTOM_SHEET_MAX_HEIGHT = ui.screenHeight * 0.6;
+  const BOTTOM_SHEET_MIN_HEIGHT = 0;
+  const MAX_UPWARD_TRANSLATE_Y = BOTTOM_SHEET_MIN_HEIGHT - BOTTOM_SHEET_MAX_HEIGHT;
+  const MAX_DOWNWARD_TRANSLATE_Y = 0;
+  const DRAG_THRESHOLD = 50;
+
+  const animatedValue = useRef(new Animated.Value(0)).current;
+  const lastGestureDy = useRef(0);
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        animatedValue.setOffset(lastGestureDy.current);
+      },
+      onPanResponderMove: (e, gesture) => {
+        animatedValue.setValue(gesture.dy);
+      },
+      onPanResponderRelease: (e, gesture) => {
+        animatedValue.flattenOffset();
+        lastGestureDy.current += gesture.dy;
+        if (lastGestureDy.current < MAX_UPWARD_TRANSLATE_Y) {
+          lastGestureDy.current = MAX_UPWARD_TRANSLATE_Y;
+        } else if (lastGestureDy.current > MAX_DOWNWARD_TRANSLATE_Y) {
+          lastGestureDy.current = MAX_DOWNWARD_TRANSLATE_Y;
+        }
+
+        if (gesture.dy > 0) {
+          if (gesture.dy <= DRAG_THRESHOLD) {
+            springAnimation('up');
+          } else {
+            springAnimation('down');
+          }
+        } else {
+          if (gesture.dy >= -DRAG_THRESHOLD) {
+            springAnimation('down');
+          } else {
+            springAnimation('up');
+          }
+        }
+      },
+    }),
+  ).current;
+
+  const springAnimation = (direction: 'up' | 'down') => {
+    console.log('direction', direction);
+    setVisible(direction === 'up');
+    lastGestureDy.current = direction === 'down' ? MAX_DOWNWARD_TRANSLATE_Y : MAX_UPWARD_TRANSLATE_Y;
+    Animated.spring(animatedValue, {
+      toValue: lastGestureDy.current,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const bottomSheetAnimation = {
+    transform: [
+      {
+        translateY: animatedValue.interpolate({
+          inputRange: [MAX_UPWARD_TRANSLATE_Y, MAX_DOWNWARD_TRANSLATE_Y],
+          outputRange: [MAX_UPWARD_TRANSLATE_Y, MAX_DOWNWARD_TRANSLATE_Y],
+          extrapolate: 'clamp',
+        }),
+      },
+    ],
+  };
+
+  useEffect(() => {
+    visible ? springAnimation('up') : springAnimation('down');
+  }, [visible]);
+
+  return (
+    <Portal>
+      <Animated.View
+        style={[
+          {
+            position: 'absolute',
+            width: '100%',
+            flex: 1,
+            bottom: BOTTOM_SHEET_MIN_HEIGHT - BOTTOM_SHEET_MAX_HEIGHT,
+            ...Platform.select({
+              android: {elevation: 3},
+              ios: {
+                shadowColor: colors.shadow,
+                shadowOpacity: 1,
+                shadowRadius: 6,
+                shadowOffset: {
+                  width: 2,
+                  height: 2,
+                },
+              },
+            }),
+            backgroundColor: '#051517',
+            borderTopLeftRadius: 32,
+            borderTopRightRadius: 32,
+          },
+          bottomSheetAnimation,
+        ]}
+      >
+        <View
+          style={{
+            width: 132,
+            height: 32,
+            alignSelf: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          {...panResponder.panHandlers}
+        >
+          <View
+            style={{
+              width: 100,
+              height: 6,
+              backgroundColor: colors.primary,
+              borderRadius: 10,
+            }}
+          />
+        </View>
+        <View
+          style={[{flex: 1, flexDirection: 'column', padding: responsiveWidth(20), gap: responsiveWidth(24)}, style]}
+        >
+          {children}
+        </View>
+      </Animated.View>
+    </Portal>
+  );
+}
--- src/components/home/CreateRoom.tsx
+++ src/components/home/CreateRoom.tsx
@@ -1,7 +1,5 @@
 import MukFAB from '../../components/custom/MukFAB';
-import MukSheet from '../../components/custom/MukSheet';
-import {useEffect, useRef} from 'react';
-import BottomSheet from '@gorhom/bottom-sheet';
+import React, {useEffect, useRef, useState} from 'react';
 import MukButton from '../custom/MukButton';
 import {useServices} from '../../services';
 import {useNavigation} from '@react-navigation/native';
@@ -16,29 +14,31 @@ import {Text, useTheme} from 'react-native-paper';
 import {MukTheme} from '../../types';
 import MukForm, {MukFormRef} from '../custom/MukForm';
 import {MainStackNavProp} from '../../navigation/MainStack';
+import MukBottomSheet from '../custom/MukBottomSheet';
 
 const CreateRoom = observer(() => {
   const {colors} = useTheme<MukTheme>();
-  const sheetRef = useRef<BottomSheet>(null);
   const navigation = useNavigation<MainStackNavProp>();
   const {api, t} = useServices();
   const {room, user} = useStores();
   const formRef = useRef<MukFormRef<IRoomConfig>>(null);
   const form: IRoomConfig | null = room.getConfig;
 
+  const [expand, setExpand] = useState(false);
+
   useEffect(() => {
     !room.getConfig?.id && api.room.setConfig();
   }, [room.getConfig]);
 
   const handleSheet = () => {
-    sheetRef.current?.expand();
+    setExpand(!expand);
   };
 
   const createRoom = async () => {
     if (formRef.current?.validateInputs()) {
       await api.room.createRoom(formRef.current?.formData() as IRoomConfig);
       if (room.isLive) {
-        sheetRef.current?.close();
+        setExpand(false);
         navigation.navigate('Room');
       }
     }
@@ -47,11 +47,7 @@ const CreateRoom = observer(() => {
   return (
     <>
       {!room.isLive ? <MukFAB onPress={handleSheet} /> : null}
-      <MukSheet
-        snaps={['44%']}
-        sheetRef={sheetRef}
-        contentStyle={{gap: responsiveWidth(24), justifyContent: 'flex-start', paddingVertical: responsiveWidth(16)}}
-      >
+      <MukBottomSheet visible={expand} setVisible={setExpand}>
         <View style={{flexDirection: 'row', gap: responsiveWidth(16)}}>
           <MukImage scale={2} source={require('../../../assets/adaptive-icon.png')} />
           <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', gap: responsiveWidth(8)}}>
@@ -88,7 +84,7 @@ const CreateRoom = observer(() => {
           </View>
         </View>
         <MukButton label={t.do('roomConfig.submit')} onPress={() => createRoom()} />
-      </MukSheet>
+      </MukBottomSheet>
     </>
   );
 });
