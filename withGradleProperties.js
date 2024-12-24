2891096384631a2df2dba0d64a282ff72da52985
Yasin Şensoy <yasin_sensoy@hotmail.com>
2024-12-24 16:42:30 +0300

    fix

---
+++ withGradleProperties.js
@@ -0,0 +1,18 @@
+const {withGradleProperties} = require('@expo/config-plugins');
+
+function withCustomGradleProperties(config) {
+  return withGradleProperties(config, function (config) {
+    const additionalGraddleProperties = [
+      // SDK 52: https://github.com/expo/expo/issues/30725
+      {type: 'property', key: 'android.enableJetifier', value: 'true'},
+    ];
+
+    additionalGraddleProperties.map(function (gradleProperty) {
+      config.modResults.push(gradleProperty);
+    });
+
+    return config;
+  });
+}
+
+module.exports = withCustomGradleProperties;
